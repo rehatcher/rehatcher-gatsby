e51450ade863a960361dee0dbb7e1761a6e57ba7
Chayapol Moemeng <mchayapol@gmail.com>
2021-12-05 08:15:34 +0700

    Added MD file reader

---
+++ contents/blog/post-2.md
@@ -0,0 +1,6 @@
+---
+slug: "/blog/my-second-post"
+date: "2021-12-05"
+title: "My second blog post"
+---
+What is going on?
---
+++ src/pages/blog/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default function IndexPage({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { allMarkdownRemark } = data
+  console.log(allMarkdownRemark)
+  const all = allMarkdownRemark.nodes.map(markdownRemark => {
+    const { date, slug, title } = markdownRemark.frontmatter
+    return (
+      <li>
+        <a href={slug}>
+          {title} {date}
+        </a>
+      </li>
+    )
+  })
+
+  return (
+    <div className="blog-post-container">
+      <ul>{all}</ul>
+    </div>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          date
+          slug
+          title
+        }
+      }
+    }
+  }
+`
