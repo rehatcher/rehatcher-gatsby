e4f2f683c9f2ca636e26be117798a602a94a72c7
narabodee <narabodee.a@gmail.com>
2021-12-28 14:25:27 +0700

    comment blog/index

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,82 +1,82 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../../components/layout"
-import {
-  BlogCard,
-  BlogCardContent,
-  BlogTitle,
-  BlogP,
-  BlogDate,
-  BlogButton,
-  BlogContainer,
-  BlogWrapper,
-} from "../../../src/components/styles/BlogStyles"
-import Img from "gatsby-image"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Layout from "../../components/layout"
+// import {
+//   BlogCard,
+//   BlogCardContent,
+//   BlogTitle,
+//   BlogP,
+//   BlogDate,
+//   BlogButton,
+//   BlogContainer,
+//   BlogWrapper,
+// } from "../../../src/components/styles/BlogStyles"
+// import Img from "gatsby-image"
 
-export default function IndexPage({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { allMarkdownRemark } = data
-  console.log(allMarkdownRemark)
+// export default function IndexPage({
+//   data, // this prop will be injected by the GraphQL query below.
+// }) {
+//   const { allMarkdownRemark } = data
+//   console.log(allMarkdownRemark)
 
-  const all = allMarkdownRemark.nodes.map(markdownRemark => {
-    const { date, slug, title, blurb, featuredImage } = markdownRemark.frontmatter
+//   const all = allMarkdownRemark.nodes.map(markdownRemark => {
+//     const { date, slug, title, blurb, featuredImage } = markdownRemark.frontmatter
 
-    return (
-      <BlogCard>
-        <BlogCardContent>
-          <BlogTitle>{title}</BlogTitle>
-          <BlogP>{blurb}</BlogP>
-          <BlogDate>{date}</BlogDate>
-          <BlogButton to={slug}>Read More</BlogButton>
-        </BlogCardContent>
-        <BlogCardContent>
-          <Img fluid={featuredImage.childImageSharp.fluid} />
-        </BlogCardContent>
-      </BlogCard>
-    )
-  })
+//     return (
+//       <BlogCard>
+//         <BlogCardContent>
+//           <BlogTitle>{title}</BlogTitle>
+//           <BlogP>{blurb}</BlogP>
+//           <BlogDate>{date}</BlogDate>
+//           <BlogButton to={slug}>Read More</BlogButton>
+//         </BlogCardContent>
+//         <BlogCardContent>
+//           <Img fluid={featuredImage.childImageSharp.fluid} />
+//         </BlogCardContent>
+//       </BlogCard>
+//     )
+//   })
 
-  return (
-    <>
-      <Layout>
-        <BlogContainer id="blog">
-          <h1
-            style={{
-              fontSize: "48px",
-              fontStyle: "normal",
-              fontWeight: "normal",
-              padding: "50px",
-              textAlign: "center",
-            }}
-          >
-            Our Blogs
-          </h1>
-          <BlogWrapper>{all}</BlogWrapper>
-        </BlogContainer>
-      </Layout>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Layout>
+//         <BlogContainer id="blog">
+//           <h1
+//             style={{
+//               fontSize: "48px",
+//               fontStyle: "normal",
+//               fontWeight: "normal",
+//               padding: "50px",
+//               textAlign: "center",
+//             }}
+//           >
+//             Our Blogs
+//           </h1>
+//           <BlogWrapper>{all}</BlogWrapper>
+//         </BlogContainer>
+//       </Layout>
+//     </>
+//   )
+// }
 
-export const pageQuery = graphql`
-  query BlogPage {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
-      nodes {
-        frontmatter {
-          date
-          slug
-          title
-          blurb
-          featuredImage {
-            childImageSharp {
-              fluid {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query BlogPage {
+//     allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+//       nodes {
+//         frontmatter {
+//           date
+//           slug
+//           title
+//           blurb
+//           featuredImage {
+//             childImageSharp {
+//               fluid {
+//                 ...GatsbyImageSharpFluid
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
