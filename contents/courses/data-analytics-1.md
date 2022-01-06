c7ea3069b3ac58dc457bc04097e0d0f408edd355
Chayapol Moemeng <mchayapol@gmail.com>
2022-01-06 12:28:00 +0700

    Added Course section

---
+++ contents/courses/data-analytics-1.md
@@ -0,0 +1,11 @@
+---
+slug: "/courses/data-analytics-1"
+date: "2021-12-24"
+title: "data-analytics-1"
+blurb: "data-analytics-1"
+language: "TH"
+author: "Chayapol Moemeng"
+featuredImage: "../blog/2021/detecting-scam-crypto.png"
+contentType: "course"
+---
+data-analytics-1
---
+++ src/templates/course-list.js
@@ -0,0 +1,109 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/layout"
+import {
+  BlogCard,
+  BlogCardContent,
+  BlogTitle,
+  BlogP,
+  BlogDate,
+  BlogButton,
+  BlogContainer,
+  BlogWrapper,
+} from "../components/styles/BlogStyles"
+
+import { GatsbyImage } from "gatsby-plugin-image"
+
+export default function CourseList({ data, pageContext }) {
+  const posts = data.allMarkdownRemark.edges.map(edge => edge.node)
+  console.log(posts)
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage =
+    currentPage - 1 === 1 ? "/blog/" : "/blog/" + (currentPage - 1).toString()
+  const nextPage = "/blog/" + (currentPage + 1).toString()
+
+  const all = posts.map(markdownRemark => {
+    const { date, slug, title, blurb, featuredImage } =
+      markdownRemark.frontmatter
+
+    return (
+      <BlogCard>
+        <BlogCardContent>
+          <BlogTitle>{title}</BlogTitle>
+          <BlogP>{blurb}</BlogP>
+          <BlogDate>{date}</BlogDate>
+          <BlogButton to={slug}>Read More</BlogButton>
+        </BlogCardContent>
+        {featuredImage && (
+          <BlogCardContent>
+            <GatsbyImage
+              image={featuredImage.childImageSharp.gatsbyImageData}
+              alt={title}
+              objectFit={"contain"}
+              style={{ height: "500px", width: "500px"}}
+            />
+          </BlogCardContent>
+        )}
+      </BlogCard>
+    )
+  })
+
+  return (
+    <>
+      <Layout>
+        <BlogContainer id="blog">
+          <h1
+            style={{
+              fontSize: "48px",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              padding: "50px",
+              textAlign: "center",
+            }}
+          >
+            Courses
+          </h1>
+          <BlogWrapper>{all}</BlogWrapper>
+        </BlogContainer>
+        <div style={{ alignItems: "center", textAlign: "center" }}>
+          <Link to={prevPage} rel="prev">
+            Previous Page
+          </Link>
+          <Link to={nextPage} rel="next">
+            Next Page
+          </Link>
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+
+export const pageQuery = graphql`
+  query courseListQuery($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      filter: { frontmatter: { contentType: { eq: "course" } } }
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date
+            slug
+            title
+            blurb
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(layout: FIXED)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
