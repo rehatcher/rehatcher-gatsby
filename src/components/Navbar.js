7c84a7461a9ebf605b93705cfb0ca1bc35bb7827
narabodeea <narabodee.a@gmail.com>
2022-01-13 10:45:55 +0700

    fix navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,54 +8,51 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="flex fixed bg-white/[0.8] items-center justify-center w-full h-16 top-0 md:px-[100px] text-[#f5862e] z-10">
-        <div className="flex items-center justify-between">
-          <div>
-            <a href="/#home" className="flex cursor-pointer px-[50px] md:px-0">
-              <img
-                src={Logo}
-                alt="Logo"
-                className="h-[40px] object-scale-down"
-              />
-            </a>
-          </div>
+      <nav className="bg-white/[0.8] flex fixed items-center justify-center w-full h-16 top-0 md:px-[100px] text-[#f5862e] z-10">
+        <div className="flex justify-between w-full">
+          <a
+            href="/#home"
+            className="flex cursor-pointer px-[50px] md:px-0"
+          >
+            <img src={Logo} alt="Logo" className="h-[40px] object-scale-down" />
+          </a>
 
           <div
             onClick={() => setOpen(!open)}
-            className="flex md:hidden cursor-pointer px-[50px] md:px-0"
+            className="flex md:hidden items-center cursor-pointer px-[50px]"
           >
             {open ? <FaTimes /> : <FaBars />}
           </div>
 
           <ul
-            className={`bg-white/[0.8] flex flex-col md:flex-row items-center absolute md:static text-center w-full transition-all duration-500 ease-in ${
-              open ? "top-full" : "top-[-1000px]"
+            className={`flex flex-col md:flex-row absolute md:static items-center w-full md:w-auto md:list-none md:text-center cursor-pointer transition-all duration-500 ease-in ${
+              open ? "top-full bg-white/[0.8]" : "top-[-1000px]"
             }`}
           >
-            <li className="cursor-pointer h-7 md:ml-8">
-              <a href="/#home">Home</a>
+            <li className="h-7 md:ml-8">
+              <a href="/#home" className="flex h-full items-center">Home</a>
             </li>
-            <li className="cursor-pointer h-7 md:ml-8">
-              <a href="/#about">About</a>
+            <li className="h-7 md:ml-8">
+              <a href="/#about" className="flex h-full items-center">About</a>
             </li>
-            <li className="cursor-pointer h-7 md:ml-8">
-              <a href="/#service">Services</a>
+            <li className="h-7 md:ml-8">
+              <a href="/#service" className="flex h-full items-center">Services</a>
             </li>
-            <li className="cursor-pointer h-7 md:ml-8">
-              <a href="/#client">Clients</a>
+            <li className="h-7 md:ml-8">
+              <a href="/#client" className="flex h-full items-center">Clients</a>
             </li>
             {DEV_MODE && (
               <>
-                <li className="cursor-pointer h-7 md:ml-8">
-                  <a href="/blog">Blogs</a>
+                <li className="h-7 md:ml-8">
+                  <a href="/blog" className="flex h-full items-center">Blogs</a>
                 </li>
-                <li className="cursor-pointer h-7 md:ml-8">
-                  <a href="/courses">Courses</a>
+                <li className="h-7 md:ml-8">
+                  <a href="/courses" className="flex h-full items-center">Courses</a>
                 </li>
               </>
             )}
-            <li className="cursor-pointer h-7 md:ml-8">
-              <a href="/#contact">Contact</a>
+            <li className="h-7 md:ml-8">
+              <a href="/#contact" className="flex h-full items-center">Contact</a>
             </li>
           </ul>
         </div>
