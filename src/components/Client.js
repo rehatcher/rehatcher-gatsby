51ee2bbd324684f73940f80e91269d2bd291be77
narabodeea <narabodee.a@gmail.com>
2022-01-26 15:40:54 +0700

    add helmet, edit client image logo

--- src/components/Client.js
+++ src/components/Client.js
@@ -13,28 +13,28 @@ function Client() {
       <div className="grid grid-cols-1 md:grid-cols-4 p-[20px] md:py-[50px]">
         <div className="flex flex-col justify-center items-center p-8">
           <img
-            className="md:h-auto object-cover max-h-36 md:max-h-52"
+            className="object-contain h-14 md:h-16"
             alt=""
             src={dastaLogo}
           />
         </div>
         <div className="flex flex-col justify-center items-center p-8">
           <img
-            className="md:h-auto object-cover max-h-36 md:max-h-52"
+            className="object-contain h-32 md:h-32"
             alt=""
             src={colpalLogo}
           />
         </div>
         <div className="flex flex-col justify-center items-center p-8">
           <img
-            className="md:h-20 object-cover max-h-36 md:max-h-52"
+            className="object-contain md:h-20 h-24"
             alt=""
             src={cysecLogo}
           />
         </div>
         <div className="flex flex-col justify-center items-center p-8">
           <img
-            className="md:h-[150px] object-cover max-h-36 md:max-h-52"
+            className="object-contain md:h-36 h-36"
             alt=""
             src={hondaLockLogo}
           />
