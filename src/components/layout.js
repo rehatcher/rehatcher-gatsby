6a7c1850626ad165ed1e8bff9b26fbff12391cbe
narabodeea <narabodee.a@gmail.com>
2022-01-28 12:33:33 +0700

    add cookiewow script

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,24 +10,45 @@ export default function Layout({ children }) {
       <Helmet>
         <meta charSet="utf-8" />
         <title>REHATCHER</title>
+        {/* <!-- Cookie Consent by https://www.cookiewow.com --> */}
+        <script
+          type="text/javascript"
+          src="https://cookiecdn.com/cwc.js"
+        ></script>
+        <script
+          id="cookieWow"
+          type="text/javascript"
+          src="https://cookiecdn.com/configs/uqtSvxnGi1UztzqTQzu4HnGT"
+          data-cwcid="uqtSvxnGi1UztzqTQzu4HnGT"
+        ></script>
       </Helmet>
       <Navbar />
       <main>{children}</main>
       <Footer />
-      <CookieConsent
+      {/* <CookieConsent
         location="bottom"
         buttonText="Accept"
         declineButtonText="Decline"
         cookieName="gatsby-gdpr-google-analytics"
+        style={{ padding: "0 75px" }}
+        enableDeclineButton
       >
-        <div className="px-[75px]">
-          <h4>เว็บไซต์นี้ใช้คุกกี้</h4>
-          เราใช้คุกกี้เพื่อเพิ่มประสิทธิภาพ
-          และประสบการณ์ที่ดีในการใช้งานเว็บไซต์
-          คุณสามารถเลือกตั้งค่าความยินยอมการใช้คุกกี้ได้ โดยคลิก
-          "การตั้งค่าคุกกี้"นโยบายความเป็นส่วนตัว
+        <div className="text-left">
+          เว็บไซต์นี้ใช้คุกกี้
+          <span>
+            เราใช้คุกกี้เพื่อเพิ่มประสิทธิภาพ
+            และประสบการณ์ที่ดีในการใช้งานเว็บไซต์
+            คุณสามารถตรวจสอบและทำความเข้าใจ{" "}
+            <a href="#" className="text-blue-300">
+              นโยบายคุกกี้
+            </a>{" "}
+            และ{" "}
+            <a href="#" className="text-blue-300">
+              นโยบายความเป็นส่วนตัว
+            </a>
+          </span>
         </div>
-      </CookieConsent>
+      </CookieConsent> */}
     </>
   )
 }
